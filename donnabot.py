96ec53db84562ebef007d45f1853e4ca9ab21aa2
BuildTools <apn@apnielsen.com>
2018-01-16 09:45:44 +0000

    added ID for second NXT brick

--- donnabot.py
+++ donnabot.py
@@ -2,7 +2,8 @@
 #
 # orginally based on examples from nxt-python/examples
 
-ID = '00:16:53:17:52:EE'
+ID1 = '00:16:53:17:52:EE'
+ID2 = '00:16:53:0A:4B:2B'
 
 import nxt, thread, time
 
@@ -14,13 +15,15 @@ import drive
 #b = nxt.find_one_brick(host=ID)
 
 from nxt.bluesock import BlueSock
-sock = BlueSock(ID)
+sock = BlueSock(ID1)
 b=sock.connect()
 
 mx = nxt.Motor(b, nxt.PORT_A) # left-side
 my = nxt.Motor(b, nxt.PORT_B) # right-side
 
 d = drive.Drive(mx,my)
+
+marm = nxt.Motor(b, nxt.PORT_C) # arm motor
 
 touch = nxt.Touch(b,nxt.PORT_1)
 ultrasonic = nxt.Ultrasonic(b,nxt.PORT_2)
