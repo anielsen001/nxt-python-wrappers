e8ab5a0dfd6163ec9b0b21a728e0859dc7286163
AAron Nielsen <apn@apnielsen.com>
2018-01-28 22:19:59 +0000

    added move arm methods

--- arm.py
+++ arm.py
@@ -15,14 +15,17 @@ class ArmError(Exception):
 
 class Arm(object):
 
+    # the motor associated with the arm
     m = None
 
     # define points where the motor must stop due to mechanical
-    # stops
+    # stops, the two values in the list should be max and min counts
     stops = list()
     
     def __init__(self,motor):
         self.m = motor
+
+        self.findStops()
 
     def findStops(self,power=20):
         # find at most two stops in the forward and back direction
@@ -31,7 +34,7 @@ class Arm(object):
         # if we already have stops set, clear them and then find the
         # stops
         if len(self.stops) >= 2:
-            self.clearstops()
+            self.clearStops()
         
         # power should be as small as possible to avoid damaging anything
         self.findStop(direction = 1, power = power)
@@ -48,19 +51,54 @@ class Arm(object):
             # the motor hit a stop - idle the motor, add the stop to the
             # stop list and return the stop value
             self.m.idle()
-            tc = self.m.get_tacho()
-            stopCount = tc.tacho_count
+            stopCount = self.getArmTacho()
             self.stops.append( stopCount )
             return stopCount
         else:
             # no stop found
             return None
-
         
-            
     def clearStops(self):
         """ clear all stops """
         self.stops = list()
 
-    
-      
+    def getArmTacho(self):
+        """ find the current tacho count of the arm """
+        tc = tc = self.m.get_tacho()
+        return tc.tacho_count
+
+    def moveArm(self,tacho,direction=1,power=100):
+        """
+        move the arm by the tacho in the given direction
+        """
+
+        # get the current arm motor location
+        currentTacho = self.getArmTacho()
+        
+        # determine the desired location to move the arm to
+        desiredTacho = tacho*np.sign( direction ) + currentTacho
+
+        # check if the desired tacho is within the stops
+        if len(self.stops) == 0:
+            # check this way to ensure we don't have index errors
+            pass
+        elif desiredTacho > self.stops[0] :
+            # exceed max
+            raise ArmError('Arm motion will exceed maximum')
+        elif desiredTacho < self.stops[1] :
+            # less than min
+            raise ArmError('Arm motion will exceed minimum')
+        else:
+            # move the arm the desired amount
+            self.m.turn(power=power*direction,tacho_units = tacho)
+        
+    def down(self,tacho,power=100):
+        """ move the arm up by tacho degrees """
+
+        self.moveArm(tacho, power = power)
+
+    def up(self, tacho, power = 100):
+        """ move the arm down by tacho degrees """
+
+        self.moveArm(tacho, power = power, direction = -1)
+        
