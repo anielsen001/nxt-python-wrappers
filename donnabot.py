5fe0a0a8e7f1f3aba8ad4aad23954eaa38e04e9c
AAron Nielsen <apn@apnielsen.com>
2018-01-28 22:20:28 +0000

    added arm movement tests

--- donnabot.py
+++ donnabot.py
@@ -7,6 +7,7 @@ import nxt, thread, time
 import numpy as np
 
 import drive
+import arm
 
 # this find_one_brick method is unreliable
 #b = nxt.find_one_brick(host=ID)
@@ -19,16 +20,27 @@ sock1 = BlueSock(ID1)
 b1 = sock1.connect()
 
 # connection to 2nd brick "pie" 
-ID2 = '00:16:53:0A:4B:2B' # MAC address
-sock2 = BlueSock(ID2)
-b2 = sock2.connect()
+#ID2 = '00:16:53:0A:4B:2B' # MAC address
+#sock2 = BlueSock(ID2)
+#b2 = sock2.connect()
 
 mx = nxt.Motor(b1, nxt.PORT_A) # left-side
 my = nxt.Motor(b1, nxt.PORT_B) # right-side
 
 d = drive.Drive(mx,my)
 
-marm = nxt.Motor(b2, nxt.PORT_C) # arm motor
+
+marm = nxt.Motor(b1, nxt.PORT_C) # arm motor
+
+a = arm.Arm(marm)
+
+marm.turn(power=-100,tacho_units=200,brake=True)
+marm.turn(power=100,tacho_units=200,brake=True)
+
+# this waved the arm around
+turn_arm = lambda x: marm.turn(power=-x,tacho_units=180,brake=True)
+[ turn_arm(_x) for _x in power ]
+power = [100,-100,100,-100]
 
 touch = nxt.Touch(b1,nxt.PORT_1)
 ultrasonic = nxt.Ultrasonic(b1,nxt.PORT_2)
